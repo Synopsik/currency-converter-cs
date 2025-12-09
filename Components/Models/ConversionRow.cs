de94163996f6385070714df74f5d44c0911316ca
Steven <synopsik@proton.me>
2025-12-09 12:45:01 -0700

    Initial commit

---
+++ Components/Models/ConversionRow.cs
@@ -0,0 +1,9 @@
+﻿namespace currency_converter_cs.Components.Models;
+
+public class ConversionRow
+{
+    public string From { get; set; }
+    public string To { get; set; }
+    public decimal Rate { get; set; }
+    public string Date { get; set; }
+}
---
+++ Components/Models/FavoritePair.cs
@@ -0,0 +1,7 @@
+﻿namespace currency_converter_cs.Components.Models;
+
+public class FavoritePair
+{
+    public string BaseCurrency { get; set; }
+    public string TargetCurrency { get; set; }
+}
